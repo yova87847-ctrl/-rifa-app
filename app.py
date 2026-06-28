471eb3e6ec9e0260c762a68dfedd736c241dfb40
yova87847-ctrl <yova87847@gmail.com>
2026-06-28 14:46:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import pandas as pd
 import os
 from fpdf import FPDF
 from datetime import datetime
+st.set_page_config(page_title="Rifa Premium", layout="wide")
+
+DB_FILE = "rifa_db.csv"
+PRECIO = 3000
+ADMIN_PASSWORD = "1234"
